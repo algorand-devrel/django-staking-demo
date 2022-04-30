a8544097444727526fcb36aede074752a5cf3fbc
nullun <git@nullun.com>
2022-04-30 17:51:07 +0100

    Basic functionality minus reward calcs

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ CREATOR="C5M4M7W32CLQA7BSEEVGDHZZDFVBCUFZEDJHBWMANWQNOXQH6TDLDCYWVE"
 
 CURRENT=$(date +"%s")
 
-BEGIN=$(expr ${CURRENT} + 30)
-END=$(expr ${BEGIN} + 3600) # 3600 seconds = 1 hour
+BEGIN=$(expr ${CURRENT} - 60)
+END=$(expr ${BEGIN} + 60) # 3600 seconds = 1 hour
 
 # Create staking asset
 STAKING_ASSET=$(${GOAL} asset create --creator ${CREATOR} \
@@ -33,15 +33,16 @@ REWARD_ASSET=$(${GOAL} asset create --creator ${CREATOR} \
 
 # Deploy new staking contract
 APP_ID=$(${GOAL} app method --create -f ${CREATOR} \
+	--on-completion "NoOp" \
 	--method "deploy(asset,asset,uint64,uint64)void" \
-	--global-byteslices 1 --global-ints 7 \
-	--local-byteslices 0 --local-ints 2 \
-	--approval-prog staking.teal \
-	--clear-prog clear.teal \
 	--arg ${STAKING_ASSET} \
 	--arg ${REWARD_ASSET} \
 	--arg ${BEGIN} \
 	--arg ${END} \
+	--global-byteslices 1 --global-ints 7 \
+	--local-byteslices 0 --local-ints 2 \
+	--approval-prog staking.teal \
+	--clear-prog clear.teal \
 	| grep 'Created app with app index' \
 	| awk '{print $6}' \
 	| tr -d '\r')
@@ -61,6 +62,7 @@ ${GOAL} clerk send -f ${CREATOR} \
 # Initialise the staking contract with minimum balance. This will have the
 # staking contract optin to the required assets.
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
+	--on-completion "NoOp" \
 	--method "init(pay,asset,asset)void" \
 	--arg minbal.txn \
 	--arg ${STAKING_ASSET} \
@@ -79,37 +81,39 @@ ${GOAL} asset send -f ${CREATOR} -t ${APP_ADDR} \
 	-o deposit.txn
 
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
-	--method "deposit(axfer)void" \
 	--on-completion "OptIn" \
+	--method "deposit(axfer)void" \
 	--arg deposit.txn
 
 # Pause Contract
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
+	--on-completion "NoOp" \
 	--method "config(bool,account)void" \
 	--arg true \
 	--arg ${CREATOR}
 
 # Unpause Contract
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
+	--on-completion "NoOp" \
 	--method "config(bool,account)void" \
 	--arg false \
 	--arg ${CREATOR}
 
 # Withdraw Staked
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
-	--method "withdraw(asset,uint64,account)void" \
 	--on-completion "NoOp" \
-	--fee 2000 \
+	--method "withdraw(asset,uint64,account)void" \
 	--arg ${STAKING_ASSET} \
-	--arg 10000000 \
-	--arg ${CREATOR}
+	--arg 0 \
+	--arg ${CREATOR} \
+	--fee 2000
 
 # Withdraw Rewards
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
-	--method "withdraw(asset,uint64,account)void" \
 	--on-completion "CloseOut" \
-	--fee 2000 \
+	--method "withdraw(asset,uint64,account)void" \
 	--arg ${STAKING_ASSET} \
-	--arg 10000000 \
-	--arg ${CREATOR}
+	--arg 0 \
+	--arg ${CREATOR} \
+	--fee 2000
 
