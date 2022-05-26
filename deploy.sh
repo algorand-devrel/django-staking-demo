01937c80d030fdc119192cad18de00bdd6930dca
nullun <git@nullun.com>
2022-05-26 20:08:38 +0100

    Updated script to run through all calls

--- deploy.sh
+++ deploy.sh
@@ -8,24 +8,26 @@ CREATOR="C5M4M7W32CLQA7BSEEVGDHZZDFVBCUFZEDJHBWMANWQNOXQH6TDLDCYWVE"
 
 CURRENT=$(date +"%s")
 
-BEGIN=$(expr ${CURRENT} - 60)
-END=$(expr ${BEGIN} + 60) # 3600 seconds = 1 hour
+BEGIN=$(expr ${CURRENT} + 25)
+END=$(expr ${BEGIN} + 325) # 3600 seconds = 1 hour
 
 # Create staking asset
+STAKING_TOTAL=1000000000000000
 STAKING_ASSET=$(${GOAL} asset create --creator ${CREATOR} \
 	--name "Staking Asset" \
 	--unitname "SA" \
-	--total 1000000000000000 \
+	--total ${STAKING_TOTAL} \
 	--decimals 6 \
 	| grep 'Created asset with asset index' \
 	| awk '{print $6}' \
 	| tr -d '\r')
 
 # Create reward asset
+REWARD_TOTAL=1000000000000
 REWARD_ASSET=$(${GOAL} asset create --creator ${CREATOR} \
 	--name "Reward Asset" \
 	--unitname "RA" \
-	--total 1000000000000 \
+	--total ${REWARD_TOTAL} \
 	--decimals 6 \
 	| grep 'Created asset with asset index' \
 	| awk '{print $6}' \
@@ -39,8 +41,8 @@ APP_ID=$(${GOAL} app method --create -f ${CREATOR} \
 	--arg ${REWARD_ASSET} \
 	--arg ${BEGIN} \
 	--arg ${END} \
-	--global-byteslices 1 --global-ints 7 \
-	--local-byteslices 0 --local-ints 2 \
+	--global-byteslices 1 --global-ints 10 \
+	--local-byteslices 0 --local-ints 3 \
 	--approval-prog staking.teal \
 	--clear-prog clear.teal \
 	| grep 'Created app with app index' \
@@ -68,11 +70,18 @@ ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
 	--arg ${STAKING_ASSET} \
 	--arg ${REWARD_ASSET}
 
-# Create asset transfer of rewards into staking contract
-${GOAL} asset send -f ${CREATOR} \
+# Create transfer of rewards to staking contract
+${GOAL} asset send --assetid ${REWARD_ASSET} -f ${CREATOR} \
 	-t ${APP_ADDR} \
-	--assetid ${REWARD_ASSET} \
-	-a 100000000
+	-a ${REWARD_TOTAL} \
+	-o rewards.txn
+# Set the fixed rate and add the rewards to contract
+${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
+	--on-completion "NoOp" \
+	--method "reward(axfer,uint64,asset)void" \
+	--arg rewards.txn \
+	--arg 100 \
+	--arg ${REWARD_ASSET}
 
 # Deposit staked asset
 ${GOAL} asset send -f ${CREATOR} -t ${APP_ADDR} \
@@ -82,8 +91,9 @@ ${GOAL} asset send -f ${CREATOR} -t ${APP_ADDR} \
 
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
 	--on-completion "OptIn" \
-	--method "deposit(axfer)void" \
-	--arg deposit.txn
+	--method "deposit(axfer,asset)void" \
+	--arg deposit.txn \
+	--arg ${STAKING_ASSET}
 
 # Pause Contract
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
@@ -99,12 +109,15 @@ ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
 	--arg false \
 	--arg ${CREATOR}
 
+echo "Sleeping for 10 seconds..."
+sleep 10
+
 # Withdraw Staked
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
 	--on-completion "NoOp" \
 	--method "withdraw(asset,uint64,account)void" \
 	--arg ${STAKING_ASSET} \
-	--arg 0 \
+	--arg 18446744073709551615 \
 	--arg ${CREATOR} \
 	--fee 2000
 
@@ -112,8 +125,8 @@ ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
 ${GOAL} app method --app-id ${APP_ID} -f ${CREATOR} \
 	--on-completion "CloseOut" \
 	--method "withdraw(asset,uint64,account)void" \
-	--arg ${STAKING_ASSET} \
-	--arg 0 \
+	--arg ${REWARD_ASSET} \
+	--arg 1844674407370955161 \
 	--arg ${CREATOR} \
 	--fee 2000
 
