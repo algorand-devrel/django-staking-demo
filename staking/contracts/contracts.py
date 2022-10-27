67eedc9260c7bf76303b153eed74981390fda264
nullun <git@nullun.com>
2022-10-27 10:10:22 +0100

    Remove personal note

--- staking/contracts/contracts.py
+++ staking/contracts/contracts.py
@@ -93,7 +93,7 @@ def send_asset(
                 TxnField.xfer_asset: asset.asset_id(),
                 TxnField.asset_amount: dispensed_amount.load(),
                 TxnField.asset_receiver: recipient.address(),
-                TxnField.fee: Int(0), # Personal note: Who covers the fee here? First appl call has fee*2?
+                TxnField.fee: Int(0),
             }
         ),
         InnerTxnBuilder.Submit(),
